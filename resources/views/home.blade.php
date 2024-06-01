1dc7522da5dc733cb07068b5ad0719e1cd661906
CristianMorales1999 <cm9225064@gmail.com>
2024-06-01 16:49:19 -0500

    2. CREACIÓN DE PLANTILLA layout.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,1 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
-</head>
-<body>
-    <h1>Home</h1>
-
-    <ul>
-        <li><a href="/"></a>Home</li>
-        <li><a href="nosotros">Nosotros</a></li>
-        <li><a href="servicios">Servicios</a></li>
-        <li><a href="contacto">Contacto</a></li>
-    </ul>
-</body>
-</html>
+@extends('layout')
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <h1>Home</h1>
+
+    <ul>
+        <li><a href="/"></a>Home</li>
+        <li><a href="nosotros">Nosotros</a></li>
+        <li><a href="servicios">Servicios</a></li>
+        <li><a href="contacto">Contacto</a></li>
+    </ul>
+</body>
+</html>
