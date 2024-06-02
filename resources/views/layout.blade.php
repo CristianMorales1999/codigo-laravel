e9eb65f61fff76aa38d5d60f65a6482da3efa5ea
CristianMorales1999 <cm9225064@gmail.com>
2024-06-01 20:31:12 -0500

    15. CREACIÓN DE ARCHIVO partials/nav.blade.php

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -16,22 +16,11 @@
 </head>
 <body>
     <nav>
-    <!--{{ dump(request()->routeIs('servicios'))}}--> <!--Este metodo devuelve true o false dependiendo de si coincide o no con la ruta mandada por parametro-->
-    <!--{{ dump(request()->route()->getName()) }}--><!--Este metodo devuelve el nombre de la ruta en la que nos encontramos-->
-
         <table class="table">
-            <thead class="table table-bordered">
-                <tr>
-                    <th scope="col" class="{{ setActivo('home')}}"><a href="/">Home</a></th>
-                    <th scope="col" class="{{ setActivo('nosotros')}}"><a href="nosotros">Nosotros</a></th>
-                    <th scope="col" class="{{ setActivo('servicios.index')}}"><a href="servicios">Servicios</a></th>
-                    <th scope="col" class="{{ setActivo('contacto')}}"><a href="contacto">Contacto</a></th>
-                </tr>
-            </thead>
+            @include('partials.nav')
         </table>
-    </nav>
-
-    <!-- Agregamos la directiva para agregar contenido dinamico-->
-    @yield('content')
+        <!-- Agregamos la directiva para agregar contenido dinamico-->
+        @yield('content')
+    </nav> 
 </body>
 </html>
