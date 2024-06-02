644c2d57b5f15deb4eb81264a6f32226ba0184e5
CristianMorales1999 <cm9225064@gmail.com>
2024-06-01 19:25:15 -0500

    10. MÉTODO only() Y except() DE resource()

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,11 @@ Route::view('contacto','contacto')->name('contacto');
 
 //Generar las 7 rutas de los metodos del controlador
 Route::resource('servicios','App\Http\Controllers\ServiciosController');
+
+//Si queremos registrar solo algunos de los metodos del controlador
+//Utizamos el metodo only() para elegir cual método se registre
+/*Route::resource('servicios','App\Http\Controllers\ServiciosController')->only('index','show');*/
+
+//Si queremos registrar todos los metodos del controlador menos algunos en especifico
+//Utizamos el metodo except() para elegir cual método no quiero que se registre
+/*Route::resource('servicios','App\Http\Controllers\ServiciosController')->only('index','show');*/
