af895587cc04a248ccff7f175354d02c929a9223
CristianMorales1999 <cm9225064@gmail.com>
2024-06-01 20:21:46 -0500

    14. USO DE HELPERS

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -22,10 +22,10 @@
         <table class="table">
             <thead class="table table-bordered">
                 <tr>
-                    <th scope="col" class="{{ request()->routeIs('home')? 'activo':''}}"><a href="/">Home</a></th>
-                    <th scope="col" class="{{ request()->routeIs('nosotros')? 'activo':''}}"><a href="nosotros">Nosotros</a></th>
-                    <th scope="col" class="{{ request()->routeIs('servicios.index')? 'activo':''}}"><a href="servicios">Servicios</a></th>
-                    <th scope="col" class="{{ request()->routeIs('contacto')? 'activo':''}}"><a href="contacto">Contacto</a></th>
+                    <th scope="col" class="{{ setActivo('home')}}"><a href="/">Home</a></th>
+                    <th scope="col" class="{{ setActivo('nosotros')}}"><a href="nosotros">Nosotros</a></th>
+                    <th scope="col" class="{{ setActivo('servicios.index')}}"><a href="servicios">Servicios</a></th>
+                    <th scope="col" class="{{ setActivo('contacto')}}"><a href="contacto">Contacto</a></th>
                 </tr>
             </thead>
         </table>
