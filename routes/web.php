8c697ed7ad0e2f798eef7a0015a37794a24c5cae
CristianMorales1999 <cm9225064@gmail.com>
2024-06-01 19:07:21 -0500

    8. CONTROLADORES, RESOURCE Y API

--- routes/web.php
+++ routes/web.php
@@ -13,16 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-$servicios=[/*
-    ['titulo'=>'Servicio 01'],
-    ['titulo'=>'Servicio 02'],
-    ['titulo'=>'Servicio 03'],
-    ['titulo'=>'Servicio 04'],
-    ['titulo'=>'Servicio 05']*/
-];
-
 Route::view('/','home')->name('home');
 Route::view('nosotros','nosotros')->name('nosotros');
-Route::view('servicios','servicios',compact('servicios'))->name('servicios');
+Route::get('servicios','App\Http\Controllers\ServiciosController@index')->name('servicios');
 Route::view('contacto','contacto')->name('contacto');
 
